8b140d6fce2b29e9394be1d0ccd70f5c14e9badd
Lucifero3 <135985879+Lucifero3@users.noreply.github.com>
2023-08-10 18:16:58 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,61 +1,29 @@
-
-using Authenticate.Models;
+using Assignment_3.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using System;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.Google;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-
-
-builder.Services.AddDbContext<ApplicationDbContext>(config =>
-{
-    config.UseInMemoryDatabase("Memory");
-
-});
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-})
-   .AddCookie()
-   .AddGoogle(options =>
-   {
-       options.ClientId = "173793821779-ghdqv90lu6s0tqtockqn8fkru8hpae31.apps.googleusercontent.com";
-       options.ClientSecret = "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";
-   });
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(config =>
-{
-    config.Password.RequiredLength = 5;
-    config.Password.RequireDigit = false;
-    config.Password.RequireNonAlphanumeric = false;
-    config.Password.RequireUppercase = false;
-})
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.ConfigureApplicationCookie(config =>
-{
-    config.Cookie.Name = "Auth_Tutorial";
-    config.LoginPath = "/Home/Login";
-
-});
-
-
-builder.Services.AddMvc();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -63,17 +31,14 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseAuthentication();
-
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-
-app.UseEndpoints(endpoint =>
-{
-    endpoint.MapDefaultControllerRoute();
-
-});
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
